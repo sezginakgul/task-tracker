ac6b328ad9cf5836f1725dbfde0efd0a873bdde5
sezgin <sezgin.akgull@gmail.com>
2023-03-17 21:07:22 +0300

    adding animation

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,17 +1,42 @@
 import React from "react";
 import TodoListItem from "./TodoListItem";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 const TodoList: React.FC<ITodoList> = ({ todos, toggleTodo, deleteTodo }) => {
+  console.log(todos);
+  const [animationParent] = useAutoAnimate();
+
   return (
-    <ul>
-      {todos.map((item) => (
+    <ul ref={animationParent}>
+      {todos.length ? (
+        todos.map((item) => (
+          <TodoListItem
+            key={item.id}
+            item={item}
+            toggleTodo={toggleTodo}
+            deleteTodo={deleteTodo}
+          />
+        ))
+      ) : (
+        <div
+          style={{
+            textAlign: "center",
+            color: "#f0ddbc",
+            fontSize: "1.3rem",
+            fontWeight: "bold",
+          }}
+        >
+          Loading...
+        </div>
+      )}
+      {/* {todos.map((item) => (
         <TodoListItem
           key={item.id}
           item={item}
           toggleTodo={toggleTodo}
           deleteTodo={deleteTodo}
         />
-      ))}
+      ))} */}
     </ul>
   );
 };
